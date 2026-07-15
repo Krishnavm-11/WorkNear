f896ceeb8a2c6cd9b14b9c3fad7e9585b8852e76
Krishnavm-11 <krishnavm013@gmail.com>
2026-07-15 13:23:15 +0530

    Configure deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,18 +1,17 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+const API = axios.create({
+  baseURL: "https://worknear-1.onrender.com",
 });
 
-api.interceptors.request.use((config) => {
+API.interceptors.request.use((config) => {
+  const user = JSON.parse(localStorage.getItem("user"));
 
-  const token = localStorage.getItem("token");
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+  if (user?.token) {
+    config.headers.Authorization = `Bearer ${user.token}`;
   }
 
   return config;
 });
 
-export default api;
+export default API;
